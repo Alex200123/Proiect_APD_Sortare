b8ed28ceb9a5969ec149c959d143a792f4791690
Alex200123 <100560843+Alex200123@users.noreply.github.com>
2023-05-02 18:39:33 +0300

    MPi_implementat

---
+++ Secvential/Functions.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+#include <chrono>
+#include <fstream>
+#include "windows.h"
+#include "psapi.h"
+using namespace std;
+using namespace std::chrono;
+
+void Odd_even_sort_odd_row(int arr[], int n);
+void Odd_even_sort_even_row(int arr[], int n);
+void Odd_even_sort_column(int** arr, int n);
+void Afiasre_matrice(int** matrice, int n);
+void Citeste_matrice(int** matrice, int n);
+void Shear_sort(int** matrice, int n);
+void Creaza_matrice_la_intamplare(int** matrice, int n);
+void Creare_fisier_matrice(ofstream& out_file);
+void Scriere_in_fisier_matrice(ofstream& out_file, int** matrice, int n);
+void Citeste_matricea_din_fisier(ifstream& in_file, int** matrice, int n);
+void PrintMemoryInfo(DWORD processID);
---
+++ Secvential/Main.cpp
@@ -0,0 +1,48 @@
+#include "Functions.h"
+
+
+
+
+int main()
+{
+    //ofstream out_file;
+    DWORD id = GetCurrentProcessId();
+
+    ifstream in_file("fisier_matrice_2560x2560.txt");
+    int n;
+    in_file >> n;
+    int** matrice = new int* [n];
+    for (int i = 0; i < n; i++)
+    {
+        matrice[i] = new int[n];
+    };
+
+    
+    Citeste_matricea_din_fisier(in_file, matrice, n);
+
+    //Afiasre_matrice(matrice, n);
+    
+    auto start = high_resolution_clock::now();
+    Shear_sort(matrice, n);
+    auto stop = high_resolution_clock::now();
+    
+    Afiasre_matrice(matrice, n);
+   
+    
+   
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << endl << "Time for function completion for matrix of " << n << "^2 is: " << duration.count() 
+         << " microseconds" << endl;
+
+    
+    PrintMemoryInfo(id);
+    
+
+    for (int i = 0; i < n; i++)
+    {
+        delete[] matrice[i];
+    }
+    delete[] matrice;
+
+	return 0;
+}
